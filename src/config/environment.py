0818d54e60e9f1525404afc0e4308d3bbb715c20
Simon Lambin <simon.lamb98@gmail.com>
2021-04-29 12:16:12 +0200

    Update docker config to remove '_' in container names

--- src/config/environment.py
+++ src/config/environment.py
@@ -5,6 +5,7 @@
 from dataclasses import dataclass, field
 import os
 
+
 @dataclass
 class Config:
     DB_HOST: str = field(default="localhost")
@@ -13,26 +14,30 @@ class Config:
     DB_PASSWORD: str = field(default="pipeline")
     DB_NAME: str = field(default="silex")
 
+
 @dataclass
 class ConfigDev(Config):
-    DB_HOST: str = field(default="silex_db_dev")
+    DB_HOST: str = field(default="silex-db-dev")
+
 
 @dataclass
 class ConfigBeta(Config):
-    DB_HOST: str = field(default="silex_db_prod")
+    DB_HOST: str = field(default="silex-db-prod")
+
 
 @dataclass
 class ConfigProd(Config):
-    DB_HOST: str = field(default="silex_db_prod")
+    DB_HOST: str = field(default="silex-db-prod")
+
 
 # Get the environment variable SILEX_BACKEND_CONFIG to set the right config
 environment_config = str(os.environ.get("SILEX_BACKEND_CONFIG")).lower()
 
-if(environment_config == "dev" or environment_config == "development"):
+if environment_config == "dev" or environment_config == "development":
     config = ConfigDev()
-elif(environment_config == "beta"):
+elif environment_config == "beta":
     config = ConfigBeta()
-elif(environment_config == "prod" or environment_config == "production"):
+elif environment_config == "prod" or environment_config == "production":
     config = ConfigProd()
 else:
     config = Config()
